10dff0661b6c1d4fafb529d108b717fbe1fafbc2
Damian Hejduk <hadook13@gmail.com>
2021-12-05 04:18:07 +0100

    Updated Vector class constructor.

--- src/vector.py
+++ src/vector.py
@@ -1,9 +1,25 @@
+from random import random, triangular
 import pygame
 
 
-# class to represent a 2d vector in-game
+# class to represent a 2d vector in-game, inherits from pygame's built-int Vector2 class
 class Vector(pygame.math.Vector2):
 
-    # returns a vector with coordinates rounded to the nearest integers
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    @property
     def int(self):
         return Vector(round(self.x), round(self.y))
+
+    @property
+    def angle(self):
+        return self.as_polar()[1]
+
+    @classmethod
+    def random(cls, max_magnitude):
+        magnitude = triangular(0, max_magnitude, max_magnitude)
+        angle = random() * 360
+        v = Vector()
+        v.from_polar((magnitude, angle))
+        return v
