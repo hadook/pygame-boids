381d844ef114d612d910801b9bfbd79a22507218
Damian Hejduk <hadook13@gmail.com>
2021-12-05 04:21:02 +0100

    -

--- src/app.py
+++ src/app.py
@@ -1,20 +1,39 @@
 import sys
 import pygame
+from vector import Vector
+from boid import Boid
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    screen_width, screen_height = 800, 600
+    screen_width, screen_height = 1600, 900
     screen = pygame.display.set_mode((screen_width, screen_height))
+
+    flock = pygame.sprite.Group()
+
+    for i in range(20):
+        boid = Boid()
+        flock.add(boid)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_SPACE:
+            #         boid.reset()
+
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_LEFT]:
+        #     boid.turn_left()
+        # if keys[pygame.K_RIGHT]:
+        #     boid.turn_right()
 
         screen.fill((30, 30, 30))
+        flock.update()
+        flock.draw(screen)
         pygame.display.flip()
         clock.tick(60)
 
--- src/boid.py
+++ src/boid.py
@@ -1,9 +1,51 @@
 import pygame
+from random import randrange
+from vector import Vector
 
 
 # Represents a single boid object
 class Boid(pygame.sprite.Sprite):
 
+    max_force = 1
+    max_speed = 1
+    art = pygame.image.load('../art/boid.png')
+
     def __init__(self):
         super().__init__()
-        
+
+        self.position = Vector()
+        self.velocity = Vector()
+
+        # self.image = pygame.transform.rotozoom(self.art)
+
+
+
+
+    #     self.art = pygame.image.load("../art/boid.png")
+    #
+    #     self.vel = 4
+    #     self.turn = 3
+    #     self.pos = Vector()
+    #     self.dir = Vector()
+    #     self.reset()
+    #
+    #     self.image = pygame.transform.rotozoom(self.art, (-1) * self.dir.as_polar()[1], 1)
+    #     self.rect = self.art.get_rect(center=self.pos)
+    #
+    # def update(self):
+    #     self.image = pygame.transform.rotozoom(self.art, (-1) * self.dir.as_polar()[1], 1)
+    #     self.pos += self.vel * self.dir
+    #     self.rect.center = self.pos.int()
+    #
+    # def reset(self):
+    #     self.pos = Vector(800, 450)
+    #     self.dir = Vector()
+    #     self.dir.randomize()
+    #     print("theta =", self.dir.as_polar()[1])
+    #
+    # def turn_left(self):
+    #     self.dir.rotate_ip(self.turn * -1)
+    #
+    # def turn_right(self):
+    #     self.dir.rotate_ip(self.turn * 1)
+
