5d26d2d14dc5567e9d74d4d94ffa1bff1a90ea20
Damian Hejduk <hadook13@gmail.com>
2021-12-10 08:07:23 +0100

    Added color masking to boids upon creation.

--- src/boid.py
+++ src/boid.py
@@ -1,4 +1,5 @@
 import pygame
+from random import triangular
 from config import Canvas
 from vector import Vector
 
@@ -9,16 +10,24 @@ class Boid(pygame.sprite.Sprite):
     max_force = 0.1
     max_speed = 4
     orientation = 300
-    art = pygame.image.load('../art/boid.png')
 
-    def __init__(self):
+    def __init__(self, color=pygame.Color('cornflowerblue')):
         super().__init__()
 
         self.position = Vector.random(max_x=Canvas.width, max_y=Canvas.height, margin=Canvas.margin)
         self.velocity = Vector.random(max_magnitude=self.max_speed)
 
+        self.color = color
+        self.art = self.load_art()
         self.image = pygame.transform.rotozoom(self.art, (-1) * self.velocity.angle, 1)
         self.rect = self.art.get_rect(center=self.position)
+
+    def load_art(self):
+        art = pygame.image.load('../art/boid.png')
+        hue = pygame.Surface(art.get_size()).convert_alpha()
+        hue.fill(self.color.correct_gamma(triangular(0.8, 1.2, 1)))
+        art.blit(hue, (0, 0), special_flags=pygame.BLEND_MULT)
+        return art
 
     def update(self):
         self.position += self.velocity
