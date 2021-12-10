b2e3b5b16c77cb50f6d7723003b69d275da50dee
Damian Hejduk <hadook13@gmail.com>
2021-12-10 23:40:09 +0100

    Updated main to include new Boid behaviour.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 import sys
 import pygame
 from config import Canvas
-from vector import Vector
 from boid import Boid
 
 
@@ -29,16 +28,22 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        if pygame.mouse.get_focused():
-            target = Vector(pygame.mouse.get_pos())
-            for b in boids:
-                b.seek(target)
+        # # seek mouse cursor
+        # if pygame.mouse.get_focused():
+        #     target = Vector(pygame.mouse.get_pos())
+        #     for b in boids:
+        #         b.seek(target)
+
+        # separation
+        for b in boids:
+            b.separate(boids)
 
         screen.fill((30, 30, 30))
         flock.update()
         flock.draw(screen)
         pygame.display.flip()
         clock.tick(60)
+        print(clock.get_fps())
 
 
 if __name__ == "__main__":
